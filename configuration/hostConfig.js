1e8d1552aef6d38f119e5e603c71b8cd08bbf2cd
AnuradhaDev96 <anusampath9470@gmail.com>
2021-12-01 02:06:18 +0530

    Refactored the urls

---
+++ configuration/hostConfig.js
@@ -0,0 +1,21 @@
+const hostingServiceAccount = {
+  "type": "service_account",
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url
+}
+
+const hostingDatabaseUrl = process.env.DATABASE_URL;
+
+module.exports = {
+  hostingServiceAccount,
+  hostingDatabaseUrl
+}
+
+
--- firestoreConfig.js
+++ firestoreConfig.js
@@ -1,18 +1,16 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
-const { getMessaging } = require('firebase-admin/messaging')
-const serviceAccount = require('./serviceAccount/serviceAccount.json');
+const { getMessaging } = require('firebase-admin/messaging');
+const hostConfig = require('./configuration/hostConfig');
 
 initializeApp({
-    credential: cert(serviceAccount),
-    databaseURL: 'https://bakery-delivery-bc6b9-default-rtdb.firebaseio.com/'
+    credential: cert(hostConfig.hostingServiceAccount),
+    databaseURL: hostConfig.hostingDatabaseUrl
 });
-
 const db = getFirestore();
 const auth = getAuth();
 const fcm = getMessaging();
-// const userI = auth.createUser()
 
 const productsRef = db.collection('Products');
 const bakeryUserRef = db.collection('BakeryUser');
